22a7a610fb3e4128783f8b76515f310918305325
Chiziii <igwechizy@gmail.com>
2020-05-19 03:56:52 +0100

    Updates

---
+++ precache-manifest.dc9a32f1c19c1d8f76dc85d398aa06bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3df1d457d87575e6ad08c30d38127c",
+    "url": "/Profiles/index.html"
+  },
+  {
+    "revision": "8a76d6575ce7fae51049",
+    "url": "/Profiles/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fe43b1f69155fd013787",
+    "url": "/Profiles/static/css/main.64441ca0.chunk.css"
+  },
+  {
+    "revision": "8a76d6575ce7fae51049",
+    "url": "/Profiles/static/js/2.e922e252.chunk.js"
+  },
+  {
+    "revision": "fe43b1f69155fd013787",
+    "url": "/Profiles/static/js/main.83edf25c.chunk.js"
+  },
+  {
+    "revision": "53b4d69e2d21243830b3",
+    "url": "/Profiles/static/js/runtime~main.93358570.js"
+  }
+]);
